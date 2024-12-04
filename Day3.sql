36b23c503c3c8de2143d76f097d05d1d536c69fb
Natalia <120486556+awariel7@users.noreply.github.com>
2024-12-04 14:44:18 +0300

    Update Day3.sql
    Fix errors

--- Day3.sql
+++ Day3.sql
@@ -11,17 +11,16 @@ with cte_menus as (
 	select cm.id
 		,cm.menu_data
 		,coalesce(cm.guests_count1, cm.guests_count2, cm.guests_count3) as Guests
-	--into temporary temp_menus	
 	from cte_menus cm  
 	-- comparison will be correct for strings
 	where coalesce(cm.guests_count1, cm.guests_count2, cm.guests_count3) > '78'
 )
 -- Get and Unnest food_item arrays
 ,cte_food as (
-	select tm.*
-		,unnest(xpath('/christmas_feast/organizational_details/menu_registry/course_details/dish_entry/food_item_id/text()', tm.menu_data)::TEXT[]) AS f_food_item_id
-		,unnest(xpath('/northpole_database/annual_celebration/event_metadata/menu_items/food_category/food_category/dish/food_item_id/text()', tm.menu_data)::TEXT[]) AS f_food_item_id2
-	from temp_menus as tm
+	select fm.*
+		,unnest(xpath('/christmas_feast/organizational_details/menu_registry/course_details/dish_entry/food_item_id/text()', fm.menu_data)::TEXT[]) AS f_food_item_id
+		,unnest(xpath('/northpole_database/annual_celebration/event_metadata/menu_items/food_category/food_category/dish/food_item_id/text()', fm.menu_data)::TEXT[]) AS f_food_item_id2
+	from cte_filtered_menus as fm
 )
 -- Calculate the most popular food_item
 select coalesce(f.f_food_item_id, f.f_food_item_id2) as food_item_id
